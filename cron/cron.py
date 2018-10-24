0892ad77f61a2dd08f7df80db2a4744c2dbfaa5c
Xinye <xinyesg@gmail.com>
2018-10-24 19:05:39 +0800

    Add front end; nginx as image; Refactor docker
    TODO: react in docker

--- cron/cron.py
+++ cron/cron.py
@@ -80,11 +80,9 @@ import os
 def email_task():
     """Just an example that sends a bell and asd to all terminals"""
     # os.system('date; echo nice;')
-    os.system('date; python3 /code/deans_api/manage.py runcrons  >> /var/log/cron.log')  
+    os.system('date; manage.py runcrons  >> /var/log/cron.log')  
 
 cron = CronTab(
-  Event(email_task, [0 ,
-1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29 ,30 ,31 ,32 ,33 ,34 ,35 ,36 ,37 ,38 ,39 ,40 ,41 ,42 ,43 ,44 ,45 ,46 ,47 ,48 ,49 ,50 ,51 ,52 ,53 ,54 ,55 ,56 ,57 ,58 ,59 ,
-  ] ),
+  Event(email_task, range(0, 60, 2) ),
 )
 cron.run()
